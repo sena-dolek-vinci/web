cd1c444075ba27199d05f62962e61a6453017726
sena-dolek-vinci <sena.dolek@student.vinci.be>
2025-11-22 12:26:29 +0100

    new 2.10

---
+++ exercices/2.10-11-12/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+interface HeaderProps {
+    urlLogo: string; 
+    children: React.ReactNode;
+}
+ 
+const Header = ( props: HeaderProps ) => {
+    return ( 
+        <footer className="header">
+            <img src={props.urlLogo} alt="logo" className="logo"/>
+            <div>{props.children}</div>
+        </footer>
+    )
+}
+export default Header;
---
+++ exercices/2.10-11-12/src/components/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import PageTitle from "../PageTitle";
+
+const HomePage = () => {
+    return (
+        <div>
+            <PageTitle title = "myMovies" />
+            <p>Welcome to myMovies , a site where you can find info about cinemas, movie...</p>
+        
+        </div>
+    );
+};
+
+export default HomePage;
